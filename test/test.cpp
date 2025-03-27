a83ace230a78d8a8793707bb4db79f8d5f074697
Tomasz Kulig <tomaszkacperkulig@gmail.com>
2025-03-27 20:34:35 +0100

    refactoring tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,4 @@
+#define UNITY_OUTPUT_CHAR(a) Serial.write(a)
 #include <unity.h>
 #include <Arduino.h>
 
@@ -6,6 +7,9 @@ void test() {
 }
 void setup() {
     Serial.begin(9600);
+
+    while (!Serial);
+    delay(1000);
 
     UNITY_BEGIN();
 
