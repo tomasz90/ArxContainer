b8b64c12f53e9074c3c028724541b2556d870c52
Tomasz Kulig <tomaszkacperkulig@gmail.com>
2025-03-29 19:16:54 +0100

    seems working 3

--- src/config/initializer_list.h
+++ src/config/initializer_list.h
@@ -3,8 +3,8 @@
 #ifndef ARX_TYPE_TRAITS_INITIALIZER_H
 #define ARX_TYPE_TRAITS_INITIALIZER_H
 
+#include <stddef.h>
 #include "has_include.h"
-
 // Initializer_list *must* be defined in std, so take extra care to only
 // define it when <initializer_list> is really not available (e.g.
 // ArduinoSTL is C++98 but *does* define <initializer_list>) and not
